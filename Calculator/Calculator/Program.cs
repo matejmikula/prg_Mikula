0e3d3c823766543ef1fbcc02daf542d276fda565
Matej Mikula <obiwan.cotlar@gmail.com>
2023-09-20 11:39:03 +0200

    uprava kalkulacky

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -15,31 +15,88 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            /*
-             * Pokud se budes chtit na neco zeptat a zrovna budu pomahat jinde, zkus se zeptat ChatGPT ;) - https://chat.openai.com/
-             * 
-             * ZADANI
-             * Vytvor program ktery bude fungovat jako kalkulacka. Kroky programu budou nasledujici:
-             * 1) Nacte vstup pro prvni cislo od uzivatele (vyuzijte metodu Console.ReadLine() - https://learn.microsoft.com/en-us/dotnet/api/system.console.readline?view=netframework-4.8.
-             * 2) Zkonvertuje vstup od uzivatele ze stringu do integeru - https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-string-to-a-number.
-             * 3) Nacte vstup pro druhe cislo od uzivatele a zkonvertuje ho do integeru. (zopakovani kroku 1 a 2 pro druhe cislo)
-             * 4) Nacte vstup pro ciselnou operaci. Rozmysli si, jak operace nazves. Muze to byt "soucet", "rozdil" atd. nebo napr "+", "-", nebo jakkoliv jinak.
-             * 5) Nadefinuj integerovou promennou result a prirad ji prozatimne hodnotu 0.
-             * 6) Vytvor podminky (if statement), podle kterych urcis, co se bude s cisly dit podle zadane operace
-             *    a proved danou operaci - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/selection-statements.
-             * 7) Vypis promennou result do konzole
-             * 
-             * Mozna rozsireni programu pro rychliky / na doma (na poradi nezalezi):
-             * 1) Vypis do konzole pred nactenim kazdeho uzivatelova vstupu co po nem chces
-             * 2) a) Kontroluj, ze uzivatel do vstupu zadal, co mel (cisla, popr. ciselnou operaci). Pokud zadal neco jineho, napis mu, co ma priste zadat a program ukoncete.
-             * 2) b) To same, co a) ale misto ukonceni programu opakovane cti vstup, dokud uzivatel nezada to, co ma
-             *       - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/iteration-statements#the-while-statement
-             * 3) Umozni uzivateli zadavat i desetinna cisla, tedy prekopej kalkulacku tak, aby umela pracovat s floaty
-             */
+            double num1, num2;
+            string a, b, operace;
+            a = Console.ReadLine();
+            b = Console.ReadLine();
+            operace = Console.ReadLine();
+            double vysledek;
+            Console.WriteLine("napiš číslo");
+            if (double.TryParse(a, out num1) && double.TryParse(b, out num2))
+            {
+                num1 = double.Parse(a);
+                num2 = double.Parse(b);
+                if (operace == "+")
+                {
+                    vysledek = num1 + num2;
+                    Console.WriteLine(vysledek);
+                }
+                else if (operace == "-")
+                {
+                    vysledek = num1 - num2;
+                    Console.WriteLine(vysledek);
+                }
+                else if (operace == "*")
+                {
+                    vysledek = num1 * num2;
+                    Console.WriteLine(vysledek);
+                }
+                else if (operace == "/")
+                {
+                    vysledek = num1 / num2;
+                    Console.WriteLine(vysledek);
+                    if (num2 == 0) ;
+                    Console.WriteLine("nulou nelze dělit");
+                }
+                else if(operace == "mocnina")
+                {
+                    vysledek = Math.Pow(num1,num2);
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Spatne");
 
-            //Tento komentar smaz a misto nej zacni psat svuj prdacky kod.
+                    return;
+                }
+                Console.WriteLine(vysledek);
 
-            Console.ReadKey(); //Toto nech jako posledni radek, aby se program neukoncil ihned, ale cekal na stisk klavesy od uzivatele.
+            }
+            else
+    
+            
+
+                
+                
+
+                /*
+                 * Pokud se budes chtit na neco zeptat a zrovna budu pomahat jinde, zkus se zeptat ChatGPT ;) - https://chat.openai.com/
+                 * 
+                 * ZADANI
+                 * Vytvor program ktery bude fungovat jako kalkulacka. Kroky programu budou nasledujici:
+                 * 1) Nacte vstup pro prvni cislo od uzivatele (vyuzijte metodu Console.ReadLine() - https://learn.microsoft.com/en-us/dotnet/api/system.console.readline?view=netframework-4.8.
+                 * 2) Zkonvertuje vstup od uzivatele ze stringu do integeru - https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-string-to-a-number.
+                 * 3) Nacte vstup pro druhe cislo od uzivatele a zkonvertuje ho do integeru. (zopakovani kroku 1 a 2 pro druhe cislo)
+                 * 4) Nacte vstup pro ciselnou operaci. Rozmysli si, jak operace nazves. Muze to byt "soucet", "rozdil" atd. nebo napr "+", "-", nebo jakkoliv jinak.
+                 * 5) Nadefinuj integerovou promennou result a prirad ji prozatimne hodnotu 0.
+                 * 6) Vytvor podminky (if statement), podle kterych urcis, co se bude s cisly dit podle zadane operace
+                 *    a proved danou operaci - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/selection-statements.
+                 * 7) Vypis promennou result do konzole
+                 * 
+                 * Mozna rozsireni programu pro rychliky / na doma (na poradi nezalezi):
+                 * 1) Vypis do konzole pred nactenim kazdeho uzivatelova vstupu co po nem chces
+                 * 2) a) Kontroluj, ze uzivatel do vstupu zadal, co mel (cisla, popr. ciselnou operaci). Pokud zadal neco jineho, napis mu, co ma priste zadat a program ukoncete.
+                 * 2) b) To same, co a) ale misto ukonceni programu opakovane cti vstup, dokud uzivatel nezada to, co ma
+                 *       - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/iteration-statements#the-while-statement
+                 * 3) Umozni uzivateli zadavat i desetinna cisla, tedy prekopej kalkulacku tak, aby umela pracovat s floaty
+                 */
+
+                //Tento komentar smaz a misto nej zacni psat svuj prdacky kod.
+
+
+
+
+                Console.ReadKey(); //Toto nech jako posledni radek, aby se program neukoncil ihned, ale cekal na stisk klavesy od uzivatele.
         }
     }
 }
