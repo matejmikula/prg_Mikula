3d1399b6b77d0e6f65cd5daed948f4c98a6d327c
matejmikula <144894481+matejmikula@users.noreply.github.com>
2023-11-15 13:43:26 +0100

    upd

--- Array_Mikula/Array_Mikula/Program.cs
+++ Array_Mikula/Array_Mikula/Program.cs
@@ -13,7 +13,17 @@ namespace Array_Mikula
         double a, b;
         a = Console.ReadLine();
         b = Console.ReadLine();
-        int [,] = int UserImput [a,b]
+        int [,] = new int [a,b]
+            for (int i = 0; i < My2dArray.GetLength(0); i++) 
+             { 
+                 for (int j = 0; j < My2dArray.GetLength(1); j++) 
+                 { 
+                     My2dArray[i,j] = i * 5 + j + 1; 
+                     Console.Write(My2dArray[i, j] + " "); 
+                 } 
+                 Console.Write("\n"); 
+             } 
+             Console.WriteLine();
         
         }
     }
