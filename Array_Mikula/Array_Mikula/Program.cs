d29c73bd63d5dc753a23e2ba6eb3cf10d8ea045d
matejmikula <144894481+matejmikula@users.noreply.github.com>
2023-11-15 13:58:53 +0100

    1. cvičení snad Done...

--- Array_Mikula/Array_Mikula/Program.cs
+++ Array_Mikula/Array_Mikula/Program.cs
@@ -11,11 +11,11 @@ namespace Array_Mikula
         static void Main(string[] args)
         {
         int a, b;
-        Console.WriteLine("Napiš zde kolik má mít list sloupců:")
-        b = Convert.ToInt(Console.ReadLine());
-        Console.WriteLine("Napiš zde kolik má mít list řádků:")
-        a = Convert.ToInt(Console.ReadLine());
-        int[,] UserArray = new int[(int)a,(int)b]
+        Console.WriteLine("Napiš zde kolik má mít list sloupců:");
+        b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Napiš zde kolik má mít list řádků:");
+        a = Convert.ToInt32(Console.ReadLine());
+        int[,] UserArray = new int[a,b];
             for (int i = 0; i < UserArray.GetLength(0); i++) 
              { 
                  for (int j = 0; j < UserArray.GetLength(1); j++) 
