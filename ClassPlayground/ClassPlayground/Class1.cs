2218a827ce9a21e7d770bfa096a46b07af0229bb
Matej Mikula <obiwan.cotlar@gmail.com>
2023-12-06 11:22:16 +0100

    čtyřúhelník skoro done

---
+++ ClassPlayground/ClassPlayground/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPlayground
+{
+    internal class Rectangle
+    {
+        public int width;
+        public int height;
+        public int CalcArea()
+        {
+            int CalcArea = width * height;
+            return CalcArea;
+        }
+        public double CalcAspectRatio() 
+        {
+            if (height == 0)
+            {
+                Console.WriteLine("Height cannot be zero.");
+                return 0.0;
+            }
+             
+            double CalcAspectRatio = (double)width / height;
+            
+            return CalcAspectRatio;
+        }
+        public int ContainsPoint()
+        {
+            int UserChoiceX = Convert.ToInt32(Console.ReadLine());
+            int UserChoiceY = Convert.ToInt32(Console.ReadLine());
+            int ContainsPoint = ;
+        }
+    }
+}
